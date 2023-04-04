e91c8f823144763717f540ca44f95872f94bf367
gjdim <gjdimpter@gmail.com>
2023-04-04 11:15:39 -0400

    Add versioning

--- src/main/java/com/mittenmc/customitems/api/CustomItemsAPI.java
+++ src/main/java/com/mittenmc/customitems/api/CustomItemsAPI.java
@@ -12,7 +12,7 @@ import javax.annotation.Nullable;
  * General api for CustomItems
  * It is accessible via {@link CustomItemsAPI#getInstance()}.
  * @author GavvyDizzle
- * @version 1.0
+ * @version 1.0.0
  */
 public class CustomItemsAPI {
 
@@ -26,7 +26,7 @@ public class CustomItemsAPI {
      * Might cause an error if this method is called when {@link CustomItems}'s startup method is still being executed.
      *
      * @return The instance of this api
-     * @since 1.0
+     * @since 1.0.0
      */
     public static CustomItemsAPI getInstance() {
         if (instance == null) {
@@ -41,6 +41,7 @@ public class CustomItemsAPI {
      * This method checks to see is this item has PDC data associated with this plugin.
      * @param itemStack The ItemStack
      * @return True if this item is a custom item, false if not
+     * @since 1.0.0
      */
     public boolean isCustomItem(ItemStack itemStack) {
         if (itemStack.getItemMeta() == null) return false;
@@ -52,6 +53,7 @@ public class CustomItemsAPI {
      * This method gets the "custom_item_id" field from this item using PDC.
      * @param itemStack The ItemStack
      * @return The item's id or null if none exists
+     * @since 1.0.0
      */
     @Nullable
     public String getCustomItemID(ItemStack itemStack) {
@@ -64,6 +66,7 @@ public class CustomItemsAPI {
      * This method checks to see if this item has a specific PDC entry.
      * @param itemStack The ItemStack
      * @return True if this item has uses, false if not
+     * @since 1.0.0
      */
     public boolean doesItemHaveUses(ItemStack itemStack) {
         return itemManager.doesCustomItemHaveUses(itemStack);
@@ -74,6 +77,7 @@ public class CustomItemsAPI {
      * This method gets the "uses_remaining" field from this item using PDC.
      * @param itemStack The ItemStack
      * @return The item's uses or -1 if it cannot be found
+     * @since 1.0.0
      */
     public int getItemUses(ItemStack itemStack) {
         return itemManager.getCustomItemUses(itemStack);
@@ -85,6 +89,7 @@ public class CustomItemsAPI {
      * @param itemStack The ItemStack
      * @param decreaseAmount The amount to decrease by
      * @return If the amount decreased successfully
+     * @since 1.0.0
      */
     public boolean decreaseItemUses(ItemStack itemStack, int decreaseAmount) {
         if (!doesItemHaveUses(itemStack)) return false;
@@ -113,6 +118,7 @@ public class CustomItemsAPI {
      * This method will do nothing if the item does not have uses.
      * @param itemStack The ItemStack
      * @return If the lore updated successfully
+     * @since 1.0.0
      */
     public boolean updateUsageLore(ItemStack itemStack) {
         if (!doesItemHaveUses(itemStack)) return false;
